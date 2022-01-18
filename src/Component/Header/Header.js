6a653aec1b5eb6e5a41c0d607f9f23770f0c0fb4
Morshedul Islam Munna <morshedulmunna1@gmail.comm>
2022-01-19 00:17:50 +0600

    Heading Add

---
+++ src/Component/Header/Header.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./Header.css";
+import logo from "../../images/logo.png";
+import { FiSearch } from "react-icons/fi";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <section className="container">
+      <nav>
+        {/* Logo Part */}
+        <div className="logo">
+          <Link to="/">
+            <img src={logo} alt="Logo" />
+            <p>CFC</p>
+          </Link>
+        </div>
+
+        {/* nav menu */}
+        <ul>
+          <li>
+            <Link to="/">Discover</Link>
+          </li>
+          <li>
+            <Link to="/cookbook">Cookbook</Link>
+          </li>
+          <li>
+            <Link to="/youtube">Youtube</Link>
+          </li>
+          <li id="login">
+            <Link to="/login">login</Link>
+          </li>
+        </ul>
+      </nav>
+      {/* Search___Bar */}
+      <div className="search__bar">
+        <FiSearch />
+        <input type="search" placeholder="search here" />
+      </div>
+    </section>
+  );
+};
+
+export default Header;
